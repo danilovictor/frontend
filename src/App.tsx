551f15a33a4e9b0afa632a13461f847edae66ab5
Danilo Victor <danilo-almeida12@hotmail.com>
2021-09-06 22:46:46 -0300

    basicstaticcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import DataTable from 'components/DataTable';
 
 
 function App() {
   return (
-    <div>
+    < >
+    <NavBar/>
+    <div className="container">
       <h1>Olá mundo teste!</h1>
+     
+      <DataTable/>
+    
+    </div>
+    
+    <Footer/>
+    </ > 
 
-    </div> 
   );
 }
 
