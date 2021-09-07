7d4b71684922a7b43df19954d90556fae0751d53
Danilo Victor <danilo-almeida12@hotmail.com>
2021-09-07 13:07:09 -0300

    StaticCharts

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,9 @@ function Footer() {
      
         <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
-          <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+          <p className="text-light">App desenvolvido por <a href="https://github.com/danilovictor" target="_blank" rel="noreferrer">DANILO VICTOR</a></p>
           <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/danilo-victor-4ab87418b/" target="_blank" rel="noreferrer">Linkedin - Danilo Victor</a></small></p>
         </div>
       </footer>
     
